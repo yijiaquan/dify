2e9fdb7f8beead62ae95efadcef90c0fc034bca3
yijiaquan <2018298805@qq.com>
2025-07-04 09:06:17 +0800

    feat: Add plugin download function
    - A new PlatformDownloadButtons component is added to facilitate the download of plugins for different platforms.
    - The download buttons are integrated into the CardWrapper.
    - The backend interface and related logic for downloading plugins are added.
    - The configuration files and environment variables are updated, and the API prefix related to plugin downloads is increased.

--- web/service/plugins.ts
+++ web/service/plugins.ts
@@ -17,6 +17,8 @@ import type {
   MarketplaceCollectionPluginsResponse,
   MarketplaceCollectionsResponse,
 } from '@/app/components/plugins/marketplace/types'
+import { MARKETPLACE_PLUGIN_INSTALL_AMD_API_PREFIX, MARKETPLACE_PLUGIN_INSTALL_ARM_API_PREFIX } from '@/config'
+import { PluginPlatform } from '@/models/common'
 
 export const uploadFile = async (file: File, isBundle: boolean) => {
   const formData = new FormData()
@@ -105,4 +107,39 @@ export const updatePermission = async (permissions: Permissions) => {
 
 export const uninstallPlugin = async (pluginId: string) => {
   return post<UninstallPluginResponse>('/workspaces/current/plugin/uninstall', { body: { plugin_installation_id: pluginId } })
+}
+
+export const downloadPlugin = async (pluginAuthor: string, pluginName: string, pluginVersion: string, platform: PluginPlatform = PluginPlatform.x86_64) => {
+  const apiPrefix = platform === PluginPlatform.x86_64 ? MARKETPLACE_PLUGIN_INSTALL_AMD_API_PREFIX : MARKETPLACE_PLUGIN_INSTALL_ARM_API_PREFIX
+
+  const url = `${apiPrefix}/repackaging/repackage`
+  const formData = new FormData()
+  formData.append('source_type', 'market')
+  formData.append('plugin_author', pluginAuthor)
+  formData.append('plugin_name', pluginName)
+  formData.append('plugin_version', pluginVersion)
+  formData.append('platform', platform)
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('console_token') || ''}`,
+    },
+    body: formData,
+  })
+
+  if (!response.ok)
+    throw new Error('Download failed')
+
+  const disposition = response.headers.get('Content-Disposition')
+  let filename = `${pluginAuthor}-${pluginName}_${pluginVersion}-offline.difypkg`
+  if (disposition && disposition.includes('filename=')) {
+    const matches = /filename="?([^"]+)"?/.exec(disposition)
+    if (matches?.[1])
+      filename = matches[1]
+  }
+
+  const blob = await response.blob()
+  const downloadUrl = window.URL.createObjectURL(blob)
+
+  return { downloadUrl, filename }
 }
